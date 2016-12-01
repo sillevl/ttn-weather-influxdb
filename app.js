e3bb57805f017af110e6137f179233415cc20099
Sille Van Landschoot <info@sillevl.be>
2016-12-01 15:26:36 +0100

    initial project

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var mqtt  = require('mqtt').connect('mqtt://192.168.1.174');
-const db = new (require('influxdb-nodejs'))('http://192.168.1.174:8086/teletask');
+var settings = require('./settings.json');
+var mqtt  = require('mqtt').connect(settings.mqtt);
+const db = new (require('influxdb-nodejs'))(settings.influxdb);
 
 mqtt.on('connect', function () {
   mqtt.subscribe('#')
@@ -8,29 +9,11 @@ mqtt.on('connect', function () {
 mqtt.on('message', function (topic, message) {
   // message is Buffer
   console.log(message.toString());
+  message = JSON.parse(message);
 
-  var measurement = topic.split('/')[0];
-  var number = topic.split('/')[1];
-  var values = {};
-  switch (measurement) {
-      case 'relay':
-          values.state = message == 'on' ? 'TRUE' : 'FALSE';
-          break;
-      case 'sensor':
-          values.temperature = Math.round( parseFloat(message) * 10) / 10;
-          break;
-      default:
-
-  }
-
-  console.log("---");
-  console.log("Measurement: " + measurement);
-  console.log("Number:      " + number);
-  console.log("Values:      " + JSON.stringify(values));
-
-  db.write(measurement)
-    .tag('number', number)
-    .field(values)
+  db.write('temperature')
+    .tag('deveui', message.deveui)
+    .field(message.fields)
     .time(Date.now(), 'ms')
     .then(() => console.info('write point success'))
     .catch(console.error);
